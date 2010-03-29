2adc78d9293203ae166bb8c33549b11642d93e6d
Andrew Kuchling <amk@amk.ca>
2010-03-29 18:20:43 -0400

    Port test suite to unittest

--- test/test_jwz.py
+++ test/test_jwz.py
@@ -3,10 +3,9 @@
 """
 Test script for jwzthreading.
 
-$Id: test_jwz.py,v 1.1 2003/03/26 13:09:56 akuchling Exp $
 """
 
-from sancho.unittest import TestScenario, parse_args, run_scenarios
+import unittest
 import jwzthreading, rfc822, StringIO
 
 tested_modules = ["jwzthreading"]
@@ -16,40 +15,33 @@ def make_rfc822_message (S):
     return rfc822.Message(input)
 
 
-class JWZTest (TestScenario):
+class JWZTest (unittest.TestCase):
 
-    def setup (self):
-        pass
-    
-    def shutdown (self):
-        pass
-
-    def check_container (self):
-        self.test_stmt('jwzthreading.Container()')
+    def test_container (self):
         c = jwzthreading.Container()
         c2 = jwzthreading.Container()
-        self.test_true('c.is_dummy()')
-        self.test_seq('c.children', [])
-        self.test_val('c.parent', None)
-        self.test_false('c.has_descendant(c2)')
+        self.assertTrue(c.is_dummy())
+        self.assertEquals(c.children, [])
+        self.assertEquals(c.parent, None)
+        self.assertFalse(c.has_descendant(c2))
 
         # Add a child
         c3 = jwzthreading.Container()
-        self.test_stmt('c.add_child(c2)')
-        self.test_stmt('c2.add_child(c3)')
-        self.test_seq('c.children', [c2])
-        self.test_val('c2.parent', c)
-        self.test_true('c.has_descendant(c2)')
-        self.test_true('c.has_descendant(c3)')
+        c.add_child(c2)
+        c2.add_child(c3)
+        self.assertEquals(c.children, [c2])
+        self.assertEquals(c2.parent, c)
+        self.assertTrue(c.has_descendant(c2))
+        self.assertTrue(c.has_descendant(c3))
 
         # Remove a child
-        self.test_stmt('c.remove_child(c2)')
-        self.test_seq('c.children', [])
-        self.test_val('c2.parent', None)
-        self.test_false('c.has_descendant(c3)')
-        self.test_true('c2.has_descendant(c3)')
+        c.remove_child(c2)
+        self.assertEquals(c.children, [])
+        self.assertEquals(c2.parent, None)
+        self.assertFalse(c.has_descendant(c3))
+        self.assertTrue(c2.has_descendant(c3))
 
-    def check_make_message (self):
+    def test_make_message (self):
         msg_templ = """Subject: %(subject)s
 Message-ID: %(msg_id)s
 
@@ -58,11 +50,10 @@ Message body
         m = make_rfc822_message("""Subject: random
 
 Body.""")        
-        self.test_exc('jwzthreading.make_message(m)', ValueError)
+        self.assertRaises(ValueError, jwzthreading.make_message, m)
         
 # class JWZTest
 
 
 if __name__ == "__main__":
-    (scenarios, options) = parse_args()
-    run_scenarios(scenarios, options)
+    unittest.main()
