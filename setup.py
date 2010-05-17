39201741c366926c5285077c6b5f592fa840e916
Andrew Kuchling <amk@amk.ca>
2010-05-16 21:09:58 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils import core
 
 kw = {
     'name' : 'jwzthreading',
-    'version' : '0.91',
+    'version' : '0.92',
     'description' : 'Algorithm for threading mail messages.',
     'long_description' : '''Contains an implementation of an algorithm for threading mail
 messages, as described at http://www.jwz.org/doc/threading.html.''',
